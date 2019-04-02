e39293feeebd6ec16e69bb80fded21fe9fb5c60f
renjiangzhe <renjiangzhe@ainirobot.com>
2019-04-02 21:37:47 +0800

    bugfix.

--- models/dbs/trading.py
+++ models/dbs/trading.py
@@ -3,7 +3,8 @@ import logging
 from mongoengine import *
 
 __all__ = (
-        'get_dyn_ticks_doc', 'get_dyn_dominant_ticks_doc', 'get_dyn_kline_doc', 'STORED_CATEGORY_LIST', 'StatisDayDoc',
+        'STORED_CATEGORY_LIST', 'KLINE_BINS_LIST',
+        'get_dyn_ticks_doc', 'get_dyn_dominant_ticks_doc', 'KlineDoc', 'StatisDayDoc',
     )
 
 # STORED_CATEGORY_LIST = ['AG', 'AL', 'AU', 'BU', 'CU', 'FU', 'HC', 'NI', 'PB', 'RB', 'RU', 'SN', 'ZN', 'WR']
