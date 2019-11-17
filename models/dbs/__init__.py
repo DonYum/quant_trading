898b041e25fc2fac4ad8be00f6d3962908e055cf
eric <eric_ren@aliyun.com>
2019-11-17 14:43:20 +0800

    Update __init__.py

--- models/dbs/__init__.py
+++ models/dbs/__init__.py
@@ -2,5 +2,6 @@
 
 from .trading import *
 from .tick_pickle import *
+from .stock_1d import *
 
-__all__ = (trading.__all__ + tick_pickle.__all__)
+__all__ = (trading.__all__ + tick_pickle.__all__ + stock_1d.__all__)
